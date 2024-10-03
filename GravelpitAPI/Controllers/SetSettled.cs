d62c1fbac4644e138d01623a1cda58284b890e17
Dawid Pawełek <czittergaming@gmail.com>
2024-10-03 19:55:49 +0200

    Update SetSettled.cs

--- GravelpitAPI/Controllers/SetSettled.cs
+++ GravelpitAPI/Controllers/SetSettled.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using MySql.Data.MySqlClient;
+using Org.BouncyCastle.Asn1.X509;
 
 namespace GravelpitAPI.Controllers
 {
@@ -8,17 +9,19 @@ namespace GravelpitAPI.Controllers
     public class SetSettled : ControllerBase
     {
         [HttpGet]
-        public string Get(string client_id, DateTime start, DateTime end, string settled)
+        public string Get(string client, DateTime start, DateTime end, string settled)
         {
             using (MySqlConnection connection = DatabaseConnection.GetConnection())
             {
                 connection.Open();
 
+                var existingClients = new GetClients().Get();
+
                 string updateQuery = "UPDATE `orders` SET `settled` = @settled WHERE `client_id` = @client_id AND `date` >= @start AND `date` <= @end";
                 using (MySqlCommand updateCommand = new MySqlCommand(updateQuery, connection))
                 {
                     updateCommand.Parameters.AddWithValue("@settled", settled);
-                    updateCommand.Parameters.AddWithValue("@client_id", client_id);
+                    updateCommand.Parameters.AddWithValue("@client_id", existingClients.FirstOrDefault(kvp => kvp.Value == client).Key);
                     updateCommand.Parameters.AddWithValue("@start", start.ToString("yyyy-MM-dd"));
                     updateCommand.Parameters.AddWithValue("@end", end.ToString("yyyy-MM-dd"));
 
